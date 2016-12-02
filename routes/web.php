b0fd5b2726a9450692baa8685ec6851e21877797
root <napestershine@gmail.com>
2016-12-02 18:57:51 +0530

    added package for form creation

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('jobs', 'JobController');
+});
