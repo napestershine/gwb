d1dd409a03818c60a9d792d90ded1487c76a926a
root <napestershine@gmail.com>
2016-12-02 22:27:30 +0530

    fixed error

--- database/migrations/2016_12_02_153216_create_messages_table.php
+++ database/migrations/2016_12_02_153216_create_messages_table.php
@@ -16,7 +16,7 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('usersn');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedInteger('job_id');
             $table->foreign('job_id')->references('id')->on('jobs');
             $table->timestamps();
